49a38f34a32cf136c4af8711f72791997559b1b5
saksham-ti <saksham.mittal@trilogy.com>
2023-01-17 22:03:55 +0000

    Added title attr

--- resources/models.py
+++ resources/models.py
@@ -8,6 +8,7 @@ class Resource(models.Model):
     environment = models.ForeignKey('services.Environment', on_delete=models.CASCADE, null=True)
     resource_type = models.CharField(max_length=128)
     created = models.BooleanField(default=False)
+    title = models.CharField(max_length=128, null=True)
     metadata_values = models.JSONField(max_length=128, null=True)
     user_input = models.JSONField(max_length=128, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
